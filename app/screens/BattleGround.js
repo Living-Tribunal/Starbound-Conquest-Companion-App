f7b5fff02053f134e11b976449582470db713c3c
Cody Dalton <cjdalton15@yahoo.com>
2025-05-01 14:00:01 -0400

    working on battleground

--- app/screens/BattleGround.js
+++ app/screens/BattleGround.js
@@ -9,7 +9,9 @@ import {
   Dimensions,
   Platform,
   SafeAreaView,
+  FlatList,
 } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
 import HeaderComponent from "@/components/header/HeaderComponent";
 import { Colors } from "../../constants/Colors";
 import { useStarBoundContext } from "../../components/Global/StarBoundProvider";
@@ -18,42 +20,154 @@ import { FIREBASE_DB, FIREBASE_AUTH } from "../../FirebaseConfig";
 
 export default function BattleGround(props) {
   const { ship } = props.route.params;
-  //console.log("Ship in BattleGround:", ship);
-
-  useEffect(() => {
-    getAllUsers();
-  }, []);
+  const [allUsers, setAllUsers] = useState(null);
+  const [allUsersShips, setAllUsersShips] = useState(null);
+  const [singleUser, setSingleUser] = useState(null);
+  const [singleUserShip, setSingleUserShip] = useState(null);
+  const [pressed, setPressed] = useState(true);
+  const [expandUserShipList, setExpandUserShipList] = useState(false);
 
   const getAllUsers = async () => {
     try {
+      const allUsersArray = [];
       const currentUserEmail = FIREBASE_AUTH.currentUser.email;
-      console.log("Current user email:", currentUserEmail);
-      if (!currentUserEmail) return;
+      if (!currentUserEmail) return [];
       const usersCollection = collection(FIREBASE_DB, "users");
       const myQuery = query(
         usersCollection,
         where("email", "!=", currentUserEmail)
       );
       const querySnapshot = await getDocs(myQuery);
-      const users = querySnapshot.docs.map((doc) => doc.data());
-      console.log("Users:", JSON.stringify(users, null, 2));
+      const users = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      allUsersArray.push(...users);
+      setAllUsers(allUsersArray);
+      //console.log("Users:", JSON.stringify(allUsersArray, null, 2));
       return users;
     } catch (e) {
       console.error("Error getting users:", e);
+      return [];
     }
   };
 
-  return (
-    <SafeAreaView style={styles.mainContainer}>
-      <HeaderComponent text="BattleGround" NavToWhere={"Player"} />
+  useEffect(() => {
+    const getAllUsersAndShips = async () => {
+      const users = await getAllUsers();
+      const usersWithShips = [];
+
+      for (const user of users) {
+        const shipCollection = collection(
+          FIREBASE_DB,
+          "users",
+          user.id,
+          "ships"
+        );
+        const shipSnapshot = await getDocs(shipCollection);
+        const ships = shipSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          userId: user.id,
+          ...doc.data(),
+        }));
+        usersWithShips.push({ ...user, allUsersShips: ships });
+      }
+      setAllUsersShips(usersWithShips);
+      //console.log("Ships:", JSON.stringify(usersWithShips, null, 2));
+    };
+    getAllUsersAndShips();
+  }, []);
+
+  const headerComponent = () => {
+    return (
       <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.title}>Battleground</Text>
         </View>
         <View style={styles.body}>
-          <ScrollView></ScrollView>
+          <View style={styles.row}>
+            <Text style={styles.text}>Your Ship</Text>
+            <Text style={styles.text}>
+              {ship.type} - {ship.shipId}
+            </Text>
+            <View style={{ justifyContent: "center", alignItems: "center" }}>
+              <Image
+                source={{ uri: ship.image }}
+                style={{ width: 150, height: 150, resizeMode: "contain" }}
+              />
+            </View>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.text}>is attacking your ship!</Text>
+          </View>
         </View>
       </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <HeaderComponent text="BattleGround" NavToWhere={"Player"} />
+      <FlatList
+        data={allUsersShips}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={headerComponent}
+        stickyHeaderIndices={[0]}
+        renderItem={({ item }) => {
+          const isPressed = item.id === expandUserShipList;
+
+          return (
+            <View>
+              <TouchableOpacity
+                style={styles.usernameToggle}
+                onPress={() => {
+                  setExpandUserShipList(isPressed ? null : item.id);
+                }}
+              >
+                <Text style={styles.usernameText}>{item.displayName}</Text>
+              </TouchableOpacity>
+
+              {isPressed &&
+                Array.isArray(item.allUsersShips) &&
+                item.allUsersShips
+                  .slice()
+                  .sort((a, b) => (a.type || "").localeCompare(b.type || ""))
+                  .map((ship) => (
+                    <TouchableOpacity
+                      onPress={() => {
+                        console.log("Pressed ship:", ship.id);
+                        setPressed((prev) => !prev);
+                      }}
+                      key={ship.id}
+                      style={{
+                        marginTop: 10,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "row",
+                      }}
+                    >
+                      {ship.image && (
+                        <Image
+                          source={{ uri: ship.image }}
+                          style={{
+                            width: 150,
+                            height: 150,
+                            resizeMode: "contain",
+                          }}
+                        />
+                      )}
+                      <Text style={styles.text}>{ship.shipId}</Text>
+                    </TouchableOpacity>
+                  ))}
+            </View>
+          );
+        }}
+        ListEmptyComponent={
+          <View style={styles.row}>
+            <Text style={styles.text}>No opponents found.</Text>
+          </View>
+        }
+      />
     </SafeAreaView>
   );
 }
@@ -83,6 +197,9 @@ const styles = StyleSheet.create({
   },
   body: {
     paddingHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
   },
   row: {
     paddingVertical: 10,
@@ -93,5 +210,29 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 16,
     textAlign: "center",
+    fontFamily: "monospace",
+    color: Colors.white,
+  },
+  flatListRenderItem: {
+    backgroundColor: Colors.green_toggle,
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  usernameText: {
+    fontSize: 20,
+    textAlign: "center",
+    fontFamily: "monospace",
+    color: Colors.white,
+  },
+  usernameToggle: {
+    backgroundColor: Colors.hudDarker,
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
